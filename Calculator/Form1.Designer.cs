afa8bbffd4247f99ae3523fb84ed038f40748898
Hexa <michal.billewicz@pollub.edu.pl>
2022-07-25 17:29:08 +0200

    Only Add function kind of works

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -41,11 +41,17 @@
             this.B7 = new System.Windows.Forms.Button();
             this.B0 = new System.Windows.Forms.Button();
             this.OutBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // plus
             // 
-            this.plus.Location = new System.Drawing.Point(503, 149);
+            this.plus.Location = new System.Drawing.Point(211, 269);
             this.plus.Name = "plus";
             this.plus.Size = new System.Drawing.Size(46, 50);
             this.plus.TabIndex = 0;
@@ -55,7 +61,7 @@
             // 
             // equals
             // 
-            this.equals.Location = new System.Drawing.Point(503, 205);
+            this.equals.Location = new System.Drawing.Point(211, 335);
             this.equals.Name = "equals";
             this.equals.Size = new System.Drawing.Size(46, 50);
             this.equals.TabIndex = 1;
@@ -65,7 +71,7 @@
             // 
             // B1
             // 
-            this.B1.Location = new System.Drawing.Point(67, 149);
+            this.B1.Location = new System.Drawing.Point(4, 269);
             this.B1.Name = "B1";
             this.B1.Size = new System.Drawing.Size(46, 50);
             this.B1.TabIndex = 2;
@@ -75,7 +81,7 @@
             // 
             // B2
             // 
-            this.B2.Location = new System.Drawing.Point(153, 149);
+            this.B2.Location = new System.Drawing.Point(73, 269);
             this.B2.Name = "B2";
             this.B2.Size = new System.Drawing.Size(46, 50);
             this.B2.TabIndex = 3;
@@ -85,7 +91,7 @@
             // 
             // B3
             // 
-            this.B3.Location = new System.Drawing.Point(235, 149);
+            this.B3.Location = new System.Drawing.Point(145, 269);
             this.B3.Name = "B3";
             this.B3.Size = new System.Drawing.Size(46, 50);
             this.B3.TabIndex = 4;
@@ -95,7 +101,7 @@
             // 
             // B6
             // 
-            this.B6.Location = new System.Drawing.Point(235, 220);
+            this.B6.Location = new System.Drawing.Point(145, 199);
             this.B6.Name = "B6";
             this.B6.Size = new System.Drawing.Size(46, 50);
             this.B6.TabIndex = 7;
@@ -105,7 +111,7 @@
             // 
             // B5
             // 
-            this.B5.Location = new System.Drawing.Point(153, 220);
+            this.B5.Location = new System.Drawing.Point(73, 199);
             this.B5.Name = "B5";
             this.B5.Size = new System.Drawing.Size(46, 50);
             this.B5.TabIndex = 6;
@@ -115,7 +121,7 @@
             // 
             // B4
             // 
-            this.B4.Location = new System.Drawing.Point(67, 220);
+            this.B4.Location = new System.Drawing.Point(4, 199);
             this.B4.Name = "B4";
             this.B4.Size = new System.Drawing.Size(46, 50);
             this.B4.TabIndex = 5;
@@ -125,7 +131,7 @@
             // 
             // B9
             // 
-            this.B9.Location = new System.Drawing.Point(235, 290);
+            this.B9.Location = new System.Drawing.Point(145, 128);
             this.B9.Name = "B9";
             this.B9.Size = new System.Drawing.Size(46, 50);
             this.B9.TabIndex = 10;
@@ -135,7 +141,7 @@
             // 
             // B8
             // 
-            this.B8.Location = new System.Drawing.Point(153, 290);
+            this.B8.Location = new System.Drawing.Point(73, 128);
             this.B8.Name = "B8";
             this.B8.Size = new System.Drawing.Size(46, 50);
             this.B8.TabIndex = 9;
@@ -145,7 +151,7 @@
             // 
             // B7
             // 
-            this.B7.Location = new System.Drawing.Point(67, 290);
+            this.B7.Location = new System.Drawing.Point(4, 128);
             this.B7.Name = "B7";
             this.B7.Size = new System.Drawing.Size(46, 50);
             this.B7.TabIndex = 8;
@@ -155,7 +161,7 @@
             // 
             // B0
             // 
-            this.B0.Location = new System.Drawing.Point(153, 357);
+            this.B0.Location = new System.Drawing.Point(73, 335);
             this.B0.Name = "B0";
             this.B0.Size = new System.Drawing.Size(46, 50);
             this.B0.TabIndex = 11;
@@ -166,16 +172,82 @@
             // OutBox
             // 
             this.OutBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.OutBox.Location = new System.Drawing.Point(67, 70);
+            this.OutBox.Location = new System.Drawing.Point(4, 34);
             this.OutBox.Name = "OutBox";
-            this.OutBox.Size = new System.Drawing.Size(482, 20);
+            this.OutBox.Size = new System.Drawing.Size(253, 20);
             this.OutBox.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 50);
+            this.button1.TabIndex = 14;
+            this.button1.Text = ".";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.b_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(46, 50);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.b_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(211, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(46, 50);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.b_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(211, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(46, 50);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "DEL";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.b_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(145, 72);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(46, 50);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "C";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.b_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(73, 72);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(46, 50);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "CE";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.b_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(664, 450);
+            this.ClientSize = new System.Drawing.Size(263, 391);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.OutBox);
             this.Controls.Add(this.B0);
             this.Controls.Add(this.B9);
@@ -192,7 +264,6 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Click += new System.EventHandler(this.b_Click);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -213,5 +284,11 @@
         private Button B7;
         private Button B0;
         private TextBox OutBox;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
     }
 }
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -5,97 +5,144 @@ namespace Calculator
         List<string> number = new List<string>();
         double result=0;
         int index = 0;
+        bool done_operation = false;
+        string operation_type = "";
+
         public Form1()
         {
             InitializeComponent();
-            number.Add("");
+            new_list_item();
             
         }
 
         private void b_Click(object sender, EventArgs e)
         {
             string choice = sender.ToString();
-            choice = choice.Substring(35 ,1);
+            choice = choice.Substring(35 ,choice.Length-35);
 
             switch (choice)
             {
                 case "+": {
-                        //when your first number is null or nan, it will skip this input
-                        if (number[0] == "") return;
-                        //when number on specific index is not null or nan, it will reset output box and increase working index;
-                        //when both numbers are not null or nan it will proceed to execute Add operation
-                        foreach(string s in number)
-                        {
-                            result += Convert.ToDouble(s);
-                        }
-                        OutBox.ResetText();
-                        OutBox.AppendText(Convert.ToString(result));
-                        //index++;
-
+                        if(basic_checker())return;
+                        operation_type = "+";
+                        //math_was_done();
+                        done_operation = true;
                         break;
                     }
                 case "-": {
-                        if (number[0] == "") return;
+                        basic_checker();
+                        operation_type = "-";
+                        math_was_done();
+                        done_operation = true;
                         break;
                     }
                 case "*": {
-                        if (number[0] == "") return;
+                        basic_checker();
+                        operation_type = "*";
+                        math_was_done();
+                        done_operation = true;
                         break;
                     }
                 case "/": {
-                        if (number[0] == "") return;
+                        basic_checker();
+                        operation_type = "/";
+                        math_was_done();
+                        done_operation = true;
+                        break;
+                    }
+                case "=":
+                    {
+                        if (number.Count < 2) return;
+                        OutBox.Text = "";
+                        OutBox.ResetText();
+                        do_math();
+                        OutBox.AppendText(Convert.ToString(result));
+                        reset_calculator();
                         break;
                     }
                 case "0":
                     {
-                        if (number[index] == "0") return;
-                        number[index] += "0";
+                        if(done_operation)after_operation();
+                        add_number_to_string(choice);
                         break;
                     }
                 case "1":
                     {
-                        if (number[index] == "0")
-                        {
-                            number[index] = "1";
-                            return;
-                        }
-                        number[index] += "1";
+                        if (done_operation) after_operation();
+                        add_number_to_string(choice);
                         break;
                     }
                 case "2":
                     {
+                        if(done_operation)after_operation();
+                        add_number_to_string(choice);
                         break;
                     }
                 case "3":
                     {
+                        if(done_operation)after_operation();
+                        add_number_to_string(choice);
                         break;
                     }
                 case "4":
                     {
+                        if(done_operation)after_operation();
+                        add_number_to_string(choice);
                         break;
                     }
                 case "5":
                     {
+                        if(done_operation)after_operation();
+                        add_number_to_string(choice);
                         break;
                     }
                 case "6":
                     {
+                        if(done_operation)after_operation();
+                        add_number_to_string(choice);
                         break;
                     }
                 case "7":
                     {
+                        if(done_operation)after_operation();
+                        add_number_to_string(choice);
                         break;
                     }
                 case "8":
                     {
+                        if(done_operation)after_operation();
+                        add_number_to_string(choice);
                         break;
                     }
                 case "9":
                     {
+                        if(done_operation)after_operation();
+                        add_number_to_string(choice);
                         break;
                     }
                 case ".":
                     {
+                        OutBox.Text = choice;
+                        break;
+                    }
+                case "C":
+                    {
+                        OutBox.Text = choice;
+                        break;
+                    }
+                case "CE":
+                    {
+                        OutBox.Text = choice;
+                        break;
+                    }
+                case "DEL":
+                    {
+                        OutBox.Text = choice;
+                        break;
+                    }
+                default:
+                    {
+                        OutBox.Text = "ERROR: " + choice;
                         break;
                     }
             }
@@ -103,11 +150,121 @@ namespace Calculator
 
         }
 
-        private void show(String text)
+
+        private void add_number_to_string(String text)
         {
-            OutBox.ResetText();
-            OutBox.AppendText(Convert.ToString(text));
+            //if index > number of elements in list, add new element to the list
+            if(index == number.Count())
+            {
+                number.Add(text);
+                return;
+            }
+
+
+            //Append string with chosen number and print result on screen
+            if (!string_is_only_0(text))
+            {
+                number[index] += text;
+                OutBox.AppendText(text);
+                return;
+            }
+            //If 
+            number[index] = text;
+            OutBox.AppendText(text);
+
         }
 
+        //checks if strings is a "0"
+        private bool string_is_only_0(string text)
+        {
+            if (number[index] == "0")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void new_list_item()
+        {
+            number.Add("0");
+        }
+        
+        private void after_operation()
+        {
+            OutBox.ResetText();
+            OutBox.Text = "";
+            done_operation = false;
+        }
+
+        private bool basic_checker()
+        {
+            if (number[0] == "") return true;
+            if (number.Count != 0)
+            {
+                new_list_item();
+                OutBox.Text = "";
+                OutBox.ResetText();
+                index++;
+                return false;
+            }
+            return false;
+        }
+
+        private void math_was_done()
+        {
+            OutBox.ResetText();
+            //OutBox.AppendText(Convert.ToString(result));
+            number.Clear();
+            number.Add(Convert.ToString(result));
+            new_list_item();
+        }
+
+        private void reset_calculator()
+        {
+            number.Clear();
+            new_list_item();
+            index = 0;
+            result = 0;
+            done_operation = true;
+        }
+
+        private void do_math()
+        {
+            switch (operation_type)
+            {
+                case "+": {
+                        //OutBox.AppendText($"{Convert.ToDouble(number[0])}+{Convert.ToDouble(number[1])}");
+                        //result = Convert.ToDouble(number[0]) + Convert.ToDouble(number[1]);
+                        foreach(String num in number)
+                        {
+                            result += Convert.ToDouble(num);
+                        }
+                        break;
+                    }
+                case "-": {
+                        OutBox.AppendText("-");
+                        result = Convert.ToDouble(number[0]) - Convert.ToDouble(number[1]);
+                        break;
+                    }
+                case "*": {
+                        OutBox.AppendText("*");
+                        result = Convert.ToDouble(number[0]) * Convert.ToDouble(number[1]);
+                        break;
+                    }
+                case "/":
+                    {
+                        OutBox.AppendText("/");
+                        result = Convert.ToDouble(number[0]) / Convert.ToDouble(number[1]);
+                        break;
+                    }
+                default:
+                    {
+                        OutBox.AppendText("YEP");
+                        break;
+                    }
+            }
+        }
+
+        
     }
 }
